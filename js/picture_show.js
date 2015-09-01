fc10ddf39fae47a94542e5a13182d094f1f5b4bf
Xiaofan CHEN <leisurelyrcxf@gmail.com>
2015-09-02 01:07:07 +0800

    change file list

--- js/picture_show.js
+++ js/picture_show.js
@@ -63,8 +63,7 @@ var scrollFlag=true
 
 
 /*file name array*/
-var picNameArray = ["0 (2).jpg", "1 (2).jpg", "10 (2).jpg", "10.jpg", "11 (2).jpg", "11.jpg", "12 (2).jpg", "12.jpg", "13 (2).jpg", "13.jpg", "14 (2).jpg", "14.jpg", "15 (2).jpg", "15.jpg", "16 (2).jpg", "16.jpg", "17 (2).jpg", "18 (2).jpg", "18.jpg", "19 (2).jpg", "19.jpg", "2 (2).jpg", "2.jpg", "20 (2).jpg", "20.jpg", "21 (2).jpg", "22 (2).jpg", "24 (2).jpg", "26 (2).jpg", "27 (2).jpg", "3 (2).jpg", "4 (2).jpg", "43 (2).jpg", "46 (2).jpg", "47.jpg", "48.jpg", "49.jpg", "5.jpg", "7 (2).jpg", "8 (2).jpg", "8.jpg", "9 (2).jpg", "9.jpg", "IMG_3089.JPG", "IMG_3102.JPG", "IMG_3139.JPG", "IMG_3166.JPG", "IMG_3222.JPG", "IMG_3238.JPG", "IMG_3244.JPG", "IMG_3245.JPG", "IMG_3262.JPG", "IMG_3269.JPG", "IMG_3270.JPG", "IMG_3275.JPG", "IMG_3310.JPG", "IMG_3315.JPG", "IMG_3318.JPG", "IMG_3322.JPG", "IMG_3329.JPG", "IMG_3335.JPG", "IMG_3341.JPG", "IMG_3342.JPG", "IMG_3343.JPG", "IMG_3344.JPG", "IMG_3345.JPG", "IMG_3346.JPG", "IMG_3379.JPG", "IMG_3382.JPG", "IMG_3383.JPG", "IMG_3384.JPG", "IMG_3473.JPG", "IMG_3479.JPG", "IMG_3482.JPG", "IMG_3483.JPG", "IMG_3499.JPG", "IMG_3527.JPG", "IMG_3532.JPG", "IMG_3559.JPG", "IMG_3582.JPG", "IMG_3635.JPG", "IMG_3636.JPG", "IMG_3646.JPG" ]
-
+var picNameArray = ["0 (2).jpg", "0.jpg", "1 (2).jpg", "1.jpg" ]
 var n=picNameArray.length
 
 
